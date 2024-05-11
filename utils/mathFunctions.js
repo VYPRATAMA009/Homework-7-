6113987e31feed834ec33cadb124ba5418a41fc9
Vinsensius Yuda Pratama <pantkrauss@gmail.com>
2024-05-11 11:13:31 +0700

    update 1
    module fungsi luas dan keliling

---
+++ utils/mathFunctions.js
@@ -0,0 +1,15 @@
+export function kelpersegipanjang(p, l) {
+    return 2 * p + 2 * l
+}
+
+export function kelpersegi(p) {
+    return 4 * p 
+}
+
+export function luaspersegipanjang(p, l) {
+    return p * l
+}
+
+export function luaspersegi(p) {
+    return p * p 
+}
